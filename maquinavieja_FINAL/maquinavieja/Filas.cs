18c2e1b2f50fb88770734ec022d64c9092948f4a
jamesgrimaldo <jamesonline5@hotmail.com>
2016-04-20 17:05:23 -0600

    version final de maquina vieja

---
+++ maquinavieja_FINAL/maquinavieja/Filas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maquinavieja
+{
+    class Filas
+    {
+       public  int[] fila;
+        public Filas()
+        {
+           fila = new int[322];
+        }
+        public void llenar ()
+        {
+            for(int i = 1; i<= 321;  )
+            {
+                fila[i] = i;
+                i++;
+            }
+
+        }
+        public void cambiar(int x)
+        {
+            for (int i = 1; i <= 321;)
+            {
+                if(fila[i] == x)
+                {
+                    fila[i] = 0;
+                }
+                
+                i++;
+            }
+        }
+
+    }
+}
